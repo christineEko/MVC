883d5614c98bbac21f5f691dd31f59450f5332af
christineEko <christine_eko33990@yahoo.com>
2019-09-08 21:31:44 +1000

    08/08/2019

--- WebApplication2/App_Start/MappingProfile.cs
+++ WebApplication2/App_Start/MappingProfile.cs
@@ -14,6 +14,14 @@ namespace WebApplication2
         {
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<CustomerDto, Customer>();
+
+            Mapper.CreateMap<Customer, CustomerDto>().ForMember(m => m.ID, opt => opt.Ignore());
+
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>();
+
+
+            Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
--- WebApplication2/Controllers/Api/MoviesController.cs
+++ WebApplication2/Controllers/Api/MoviesController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApplication2.Dtos;
+using WebApplication2.Models;
+using AutoMapper;
 
 namespace WebApplication2.Controllers.Api
 {
@@ -16,8 +19,70 @@ namespace WebApplication2.Controllers.Api
             _context = new MyDbContext();
         }
 
-        public IEnumerable<
-            >
+        public IEnumerable<MovieDto> GetMovies()
+        {
+            return _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+        }
+
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movie, MovieDto>(movieInDb));
+
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDto movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var newMovie = Mapper.Map<MovieDto, Movie>(movie);
+
+            _context.Movies.Add(newMovie);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/" + newMovie.Id), movie);
+
+        }
+
+        [HttpPut]
+        public IHttpActionResult UpdateMovie(int id, MovieDto movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+
+            Mapper.Map(movie, movieInDb);
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 
 }
